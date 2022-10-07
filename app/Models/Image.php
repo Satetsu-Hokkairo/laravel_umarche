62061c9ca019daa5ab6924bd743e588daa0714fa
Satetsu-Hokkairo <satetu.hokkairo@gmail.com>
2022-10-07 10:51:32 +0900

    imageIndex

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
 
 class Image extends Model
 {
@@ -11,6 +12,11 @@ class Image extends Model
 
     protected $fillable = [
         'owner_id',
-        'filename',
+        'filename'
     ];
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
 }
--- resources/views/components/shop-thumbnail.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if(empty($filename))
-     <img src="{{asset('images/no_image.jpg')}}" alt="product-image-none">
-    @else
-     <img src="{{  asset('storage/shops/' . $filename)  }}" alt="product-image">
-    @endif
-</div>
